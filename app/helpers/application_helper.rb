673517448057b2f0577f1ed100e2f790b1104ed5
Bhuvan Agrawal <bhuvan002@gmail.com>
2017-04-02 20:27:48 +0530

    Finish layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(name="")
+    base_title = "Equipment Tracker"
+    if name.blank?
+      title = base_title   
+    else
+      title = "#{name} | #{base_title}"
+    end
+  end
 end
