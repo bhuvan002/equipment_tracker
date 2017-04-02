959be622a32ce69c1bb1ceec3bf89ef1674502ca
Bhuvan Agrawal <bhuvan002@gmail.com>
2017-04-02 22:36:40 +0530

    Add devise roles for other users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get '/sign_in', to: 'devise/sessions#new'
-    get '/sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
-  end
+  # devise_scope :user do
+  #   get '/sign_in', to: 'devise/sessions#new'
+  #   get '/sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
+  # end
 
-  devise_for :users
+  devise_for :users, :store_managers, :lab_owners
   root 'static_pages#home'
 
   resources :users, only: [:show]
